8e6e2c377f1a6b0d93f4ca956dd243f9869d84d6
anthonyfw <anthonyfletcherw@gmail.com>
2022-02-23 19:50:33 -0500

    Editing js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ let h1 = document.querySelector('h1');
 console.log(h1);
 console.log(h1.parentElement);
 console.log(h1.children); //children returns an empty list
+
+let ul = document.querySelector('ul');
+console.log(ul.children);
+console.log(ul.lastElementChild);
+console.log(ul.lastElementChild.childNodes[0].nodeType);
